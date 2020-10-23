025b8fe18435198b0953f0a870e7b77c01af227c
Rounaq Jhunjhunu Wala <rounaqwl66@gmail.com>
2020-10-23 08:27:53 +0530

    AutoPEP8 the code
    - For better code-reading and maintenance

--- kingdra_cluster/ladder_utils.py
+++ kingdra_cluster/ladder_utils.py
@@ -1,6 +1,6 @@
 
 import keras
-from keras.models import * 
+from keras.models import *
 from keras.layers import *
 import keras.backend as K
 
@@ -11,53 +11,48 @@ import tensorflow as tf
 import random
 
 
-class AddBeta( Layer):
-    def __init__(self  , **kwargs):
+class AddBeta(Layer):
+    def __init__(self, **kwargs):
         super(AddBeta, self).__init__(**kwargs)
-        
+
     def build(self, input_shape):
-        
+
         if self.built:
             return
-        
-        self.beta = self.add_weight(name='beta', 
-                                      shape= input_shape[1:] ,
-                                      initializer='zeros',
-                                      trainable=True)
+
+        self.beta = self.add_weight(name='beta',
+                                    shape=input_shape[1:],
+                                    initializer='zeros',
+                                    trainable=True)
         self.built = True
 
-        super(AddBeta, self).build(input_shape)  
-        
-    def call(self, x , training=None):
-        return tf.add( x  ,  self.beta )
-    
+        super(AddBeta, self).build(input_shape)
 
-    
-    
-    
-    
+    def call(self, x, training=None):
+        return tf.add(x,  self.beta)
+
+
 class G_Guass(Layer):
 
-    def __init__(self , **kwargs):
+    def __init__(self, **kwargs):
         super(G_Guass, self).__init__(**kwargs)
-        
-        
-    def wi(self ,  init , name):
+
+    def wi(self,  init, name):
         if init == 1:
-            return self.add_weight(name='guess_'+name, 
-                                      shape=( self.size, ),
-                                      initializer='ones',
-                                      trainable=True)
+            return self.add_weight(name='guess_'+name,
+                                   shape=(self.size, ),
+                                   initializer='ones',
+                                   trainable=True)
         elif init == 0:
-            return self.add_weight(name='guess_'+name, 
-                                      shape=( self.size, ),
-                                      initializer='zeros',
-                                      trainable=True)
+            return self.add_weight(name='guess_'+name,
+                                   shape=(self.size, ),
+                                   initializer='zeros',
+                                   trainable=True)
 
     def build(self, input_shape):
 
         self.size = input_shape[0][-1]
-        
+
         self.a1 = self.wi(0., 'a1')
         self.a2 = self.wi(1., 'a2')
         self.a3 = self.wi(0., 'a3')
@@ -69,46 +64,38 @@ class G_Guass(Layer):
         self.a8 = self.wi(0., 'a8')
         self.a9 = self.wi(0., 'a9')
         self.a10 = self.wi(0., 'a10')
-        
-        super( G_Guass , self).build(input_shape)   
+
+        super(G_Guass, self).build(input_shape)
 
     def call(self, x):
-        z_c, u = x 
-        
-        a1 = self.a1 
-        a2 = self.a2 
-        a3 = self.a3 
-        a4 = self.a4 
-        a5 = self.a5 
-        a6 = self.a6 
-        a7 = self.a7 
-        a8 = self.a8 
-        a9 = self.a9 
-        a10 =self.a10
-        
+        z_c, u = x
+
+        a1 = self.a1
+        a2 = self.a2
+        a3 = self.a3
+        a4 = self.a4
+        a5 = self.a5
+        a6 = self.a6
+        a7 = self.a7
+        a8 = self.a8
+        a9 = self.a9
+        a10 = self.a10
+
         mu = a1 * tf.sigmoid(a2 * u + a3) + a4 * u + a5
         v = a6 * tf.sigmoid(a7 * u + a8) + a9 * u + a10
 
         z_est = (z_c - mu) * v + mu
         return z_est
-    
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0][0], self.size)
 
 
-
-def add_noise( inputs , noise_std ):
-    return Lambda( lambda x: x + tf.random_normal(tf.shape(x)) * noise_std  )( inputs )
-
+def add_noise(inputs, noise_std):
+    return Lambda(lambda x: x + tf.random_normal(tf.shape(x)) * noise_std)(inputs)
 
 
 def batch_normalization(batch, mean=None, var=None):
     if mean is None or var is None:
         mean, var = tf.nn.moments(batch, axes=[0])
     return (batch - mean) / tf.sqrt(var + tf.constant(1e-10))
-
-
-
-    
-    
--- kingdra_cluster/unsup_losses.py
+++ kingdra_cluster/unsup_losses.py
@@ -1,16 +1,15 @@
 
 import keras
-from keras.models import * 
+from keras.models import *
 from keras.layers import *
 import keras.backend as K
 import numpy as np
 import tensorflow as tf
 
 
-
-
 def entropy(p):
     return -tf.reduce_sum(p * tf.log(p + 1e-16), axis=1)
+
 
 def compute_kld(p_logit, q_logit):
     p = tf.nn.softmax(p_logit)
@@ -18,106 +17,87 @@ def compute_kld(p_logit, q_logit):
     return tf.reduce_sum(p*(tf.log(p + 1e-16) - tf.log(q + 1e-16)), axis=1)
 
 
-
-
-
-def mut_inf_loss( p_logit , mu ):
+def mut_inf_loss(p_logit, mu):
     p = tf.nn.softmax(p_logit)
     p_ave = tf.reduce_mean(p, axis=0)
     loss_eq2 = -tf.reduce_sum(p_ave * tf.log(p_ave + 1e-16))
     loss_eq1 = tf.reduce_mean(entropy(p))
     loss_eq = loss_eq1 - mu * loss_eq2
     return loss_eq
-    
-    
-    
-    
-    
 
-def normalize_l2( x):
-    y = K.pow(K.sum(x**2, axis = 1), 0.5)
-    y = K.expand_dims( y ,  1)
-    y = K.repeat_elements(y, x.shape[1], axis = 1)
+
+def normalize_l2(x):
+    y = K.pow(K.sum(x**2, axis=1), 0.5)
+    y = K.expand_dims(y,  1)
+    y = K.repeat_elements(y, x.shape[1], axis=1)
     return x/y
 
 
-
-def self_dot_loss( p_logit , l='mse' ):
+def self_dot_loss(p_logit, l='mse'):
     p = tf.nn.softmax(p_logit)
-    p = normalize_l2( p )
+    p = normalize_l2(p)
     d = K.dot(p, K.transpose(p))
-    print( "d.shape" ,  d.shape)
-    y = tf.eye( K.shape(d)[0] )
-    
-    if l == 'bce':
-        return K.mean( keras.metrics.binary_crossentropy( y , d ) )
-    elif l == 'mse':
-        return K.mean( keras.metrics.mean_squared_error( y , d ) )
-    
-    
-    
-def eye_dot_loss( p_logit , p_logit2 , l='mse' ):
-    p = tf.nn.softmax(p_logit)
-    p = normalize_l2( p )
-    
-    p2 = tf.nn.softmax(p_logit2 )
-    p2 = normalize_l2( p2 )
-    
-    d = K.dot(p, K.transpose(p2 ))
-    print( "d.shape" ,  d.shape)
-    y = tf.eye( K.shape(d)[0] )
-    
-    if l == 'bce':
-        return K.mean( keras.metrics.binary_crossentropy( y , d ) )
-    elif l == 'mse':
-        return K.mean( keras.metrics.mean_squared_error( y , d ) )
-    
-    
+    print("d.shape",  d.shape)
+    y = tf.eye(K.shape(d)[0])
 
-def random_noise_loss(x, ul_logits ):
-    
+    if l == 'bce':
+        return K.mean(keras.metrics.binary_crossentropy(y, d))
+    elif l == 'mse':
+        return K.mean(keras.metrics.mean_squared_error(y, d))
+
+
+def eye_dot_loss(p_logit, p_logit2, l='mse'):
+    p = tf.nn.softmax(p_logit)
+    p = normalize_l2(p)
+
+    p2 = tf.nn.softmax(p_logit2)
+    p2 = normalize_l2(p2)
+
+    d = K.dot(p, K.transpose(p2))
+    print("d.shape",  d.shape)
+    y = tf.eye(K.shape(d)[0])
+
+    if l == 'bce':
+        return K.mean(keras.metrics.binary_crossentropy(y, d))
+    elif l == 'mse':
+        return K.mean(keras.metrics.mean_squared_error(y, d))
+
+
+def random_noise_loss(x, ul_logits):
+
     d = tf.random_normal(shape=tf.shape(x))
-    d /= (tf.reshape(tf.sqrt(tf.reduce_sum(tf.pow(d, 2.0), axis=1)), [-1, 1]) + 1e-16)
+    d /= (tf.reshape(tf.sqrt(tf.reduce_sum(tf.pow(d, 2.0), axis=1)),
+                     [-1, 1]) + 1e-16)
     ul_logits = tf.stop_gradient(ul_logits)
     y1 = ul_logits
-    y2 = enc(x + d )
+    y2 = enc(x + d)
     return tf.reduce_mean(compute_kld(y1, y2))
 
 
-
-
-    
-def self_dot_loss_augment( p_logit , l='mse' ):
+def self_dot_loss_augment(p_logit, l='mse'):
     p = tf.nn.softmax(p_logit)
-    p = normalize_l2( p )
+    p = normalize_l2(p)
     d = K.dot(p, K.transpose(p))
-    print( "d.shape" ,  d.shape)
-    y = tf.eye( K.shape(d)[0] )
-    
+    print("d.shape",  d.shape)
+    y = tf.eye(K.shape(d)[0])
+
     if l == 'bce':
-        return K.mean( keras.metrics.binary_crossentropy( y , d ) )
+        return K.mean(keras.metrics.binary_crossentropy(y, d))
     elif l == 'mse':
-        return K.mean( keras.metrics.mean_squared_error( y , d ) )
+        return K.mean(keras.metrics.mean_squared_error(y, d))
 
 
-    
-    
-def zero(a , b ):
-    return K.mean( a + b )*0
+def zero(a, b):
+    return K.mean(a + b)*0
 
 
-
-def virtual_randnoise_loss(x, ul_logits, enc , rnd_std_dev=0.5  ):
+def virtual_randnoise_loss(x, ul_logits, enc, rnd_std_dev=0.5):
     r_vadv = rnd_std_dev*tf.random_normal(shape=tf.shape(x))
     ul_logits = tf.stop_gradient(ul_logits)
     y1 = ul_logits
-    y2 = enc(x + r_vadv )
+    y2 = enc(x + r_vadv)
     return tf.reduce_mean(compute_kld(y1, y2))
 
 
-
-
-def get_randnoise_loss( inp , logits, enc , rnd_std_dev=0.5  ) :
-    return virtual_randnoise_loss( inp , logits, enc , rnd_std_dev=rnd_std_dev  )
-
-
+def get_randnoise_loss(inp, logits, enc, rnd_std_dev=0.5):
+    return virtual_randnoise_loss(inp, logits, enc, rnd_std_dev=rnd_std_dev)
